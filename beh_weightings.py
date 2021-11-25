2776aef82b7e0aff9c2a14cc3c9291e21ec57099
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2021-11-25 18:42:30 +0100

    add new script for misc beh analyses

--- beh_weightings.py
+++ beh_weightings.py
@@ -9,16 +9,6 @@ import seaborn as sns
 from config import ANALYSIS_DIR, BAD_SUBJS, DATA_DIR_LOCAL, get_sourcedata
 
 # %%
-# Check how often participants "timed out" on their choices
-for sub in range(1, 33):
-    for stream in ["single", "dual"]:
-        _, tsv = get_sourcedata(sub, stream, DATA_DIR_LOCAL)
-        df = pd.read_csv(tsv, sep="\t")
-        validity_sum = df["validity"].to_numpy().sum()
-        if validity_sum != df.shape[0]:
-            print(sub, stream, f" - n timeouts: {df.shape[0]-validity_sum}")
-
-# %%
 
 
 def calc_nonp_weights(df):
