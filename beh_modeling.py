e5b74c555b3219d9de740067f01d17f482c76178
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2022-02-04 18:06:00 +0100

    make dir AFTER parsing dir

--- beh_modeling.py
+++ beh_modeling.py
@@ -36,7 +36,6 @@ analysis_dir = ANALYSIS_DIR_LOCAL
 data_dir = DATA_DIR_LOCAL
 
 fname_estimates = analysis_dir / "derived_data" / f"estim_params_{minimize_method}.tsv"
-fname_estimates.parent.mkdir(parents=True, exist_ok=True)
 
 # %%
 # When not in an IPython session, get command line inputs
@@ -51,6 +50,10 @@ if not hasattr(sys, "ps1"):
 
     analysis_dir = defaults["analysis_dir"]
     data_dir = defaults["data_dir"]
+
+# %%
+# Make dirs that don't exist yet
+fname_estimates.parent.mkdir(parents=True, exist_ok=True)
 
 # %%
 # fit model
